dbbfd6b3ad0aa14053126366d14bd9219d785481
nostra <141655452+nostrainu@users.noreply.github.com>
2025-06-28 18:11:25 +0800

    Update rejoin.py

--- misc/rejoin.py
+++ misc/rejoin.py
@@ -2,7 +2,7 @@
 # grant rejoin tool – public beta
 # please donate ❤  (GCash / PayPal)
 
-__version__ = "3.3"
+__version__ = "3.4"
 
 RAW_URL = ("https://raw.githubusercontent.com/nostrainu/dumps/"
            "refs/heads/main/misc/rejoin.py")
